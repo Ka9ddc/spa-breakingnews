8f257060825b173fc2deec1cdca4af20e8b5b0d3
Ryan Micael <ka9ddcgamer@gmail.com>
2023-02-15 19:59:40 -0300

    refactor: card styled with styled-components

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,17 @@
+import { Card } from "../../components/Cards/Card";
 import { Navbar } from "../../components/Navbar/Navbar";
+import { news } from "../../Datas.js";
+import { HomeBody } from "./HomeStyled";
 
 export default function Home() {
   return (
     <>
       <Navbar />
-      <h1>Olá Mundo</h1>
+      <HomeBody>
+        {news.map((item, index) => (
+          <Card key={index} news={item} />
+        ))}
+      </HomeBody>
     </>
   );
 }
